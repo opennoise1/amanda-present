74f118e22bbfbac59d55f40e78c3537fb4263eb1
Chris <ckopcow@gmail.com>
2026-04-08 02:50:01 -0400

    the final commit?

--- components/Button.tsx
+++ components/Button.tsx
@@ -133,7 +133,7 @@ const Button = ({question, setQuestion, answered, setAnswered, title, setAnswerC
     } else {
       return (
         <>
-          <button onClick={nextQuestionAndToggleAnswered} className='fontSans navButton'>
+          <button id="resultsButton" onClick={nextQuestionAndToggleAnswered} className='fontSans navButton'>
             <svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true" className="h-14 w-14 sm:h-20 sm:w-20">
               <path fill="currentColor" fillRule="evenodd" d="M8.048 2.488a.75.75 0 0 1-.036 1.06l-4.286 4a.75.75 0 0 1-1.095-.076l-1.214-1.5a.75.75 0 0 1 1.166-.944l.708.875 3.697-3.451a.75.75 0 0 1 1.06.036M11.25 5a.75.75 0 0 1 .75-.75h10a.75.75 0 0 1 0 1.5H12a.75.75 0 0 1-.75-.75M8.048 9.488a.75.75 0 0 1-.036 1.06l-4.286 4a.75.75 0 0 1-1.095-.076l-1.214-1.5a.75.75 0 1 1 1.166-.944l.708.875 3.697-3.451a.75.75 0 0 1 1.06.036M11.25 12a.75.75 0 0 1 .75-.75h10a.75.75 0 0 1 0 1.5H12a.75.75 0 0 1-.75-.75m-3.202 4.488a.75.75 0 0 1-.036 1.06l-4.286 4a.75.75 0 0 1-1.095-.076l-1.214-1.5a.75.75 0 1 1 1.166-.944l.708.875 3.697-3.451a.75.75 0 0 1 1.06.036M11.25 19a.75.75 0 0 1 .75-.75h10a.75.75 0 0 1 0 1.5H12a.75.75 0 0 1-.75-.75" clipRule="evenodd">
               </path>
